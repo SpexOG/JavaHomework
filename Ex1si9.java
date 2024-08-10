f8bdccd8978d41702c1389da0d8a2169ef76d9a6
Sebastian Mutulescu <sebastian.mutulescu9@gmail.com>
2024-08-10 14:37:25 +0300

    Java operators exercitii

---
+++ Ex1si9.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Ex1si9 {
+    public static void main(String[] args) {
+
+        double arieDreptunghi;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduceti cele doua numere");
+        System.out.println("Introduceti primul numar: ");
+
+        double primulNumar = scanner.nextDouble();
+
+        System.out.println("Introduceti al doilea numar: ");
+
+        double alDoileaNumar = scanner.nextDouble();
+
+        System.out.println("Adunearea celor doua numere are rezultatul: " + (primulNumar + alDoileaNumar));
+        System.out.println("Scaderea celor doua numere are rezultatul: " + (primulNumar - alDoileaNumar));
+        System.out.println("Inmultirea celor doua numere are rezultatul: " + (primulNumar * alDoileaNumar));
+        System.out.println("Impartirea celor doua numere are rezultatul: " + (primulNumar / alDoileaNumar));
+        System.out.println("Modulo celor doua numere are rezultatul: " + (primulNumar % alDoileaNumar));
+
+        System.out.println("Introduceti lungimea si latimea dreptunghiului pentru calcularea ariei acestuia: ");
+        System.out.println("Lungimea dreptunghiului: ");
+
+        double lungimeDreptunghi = scanner.nextDouble();
+
+        System.out.println("Latimea dreptunghiului: ");
+
+        double latimeDreptunghi = scanner.nextDouble();
+
+        if (lungimeDreptunghi > latimeDreptunghi)
+        System.out.println("Aria dreptunghiului este egala cu " + (lungimeDreptunghi * latimeDreptunghi) + " " + "unitati patrate.");
+        else
+            System.out.println("Latimea nu poate fi mai mare decat lungimea!");
+
+        scanner.close();
+    }
+}
---
+++ Ex3si11.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ex3si11 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introducere numar: ");
+
+        int numar = scanner.nextInt();
+        boolean paritate = numar < 0;
+
+        if (!paritate && numar != 0)
+            System.out.println("Numarul este pozitiv!");
+        else if (numar == 0) {
+            System.out.println("Numarul este 0!");
+        }else
+            System.out.println("Numarul este negativ!");
+
+        System.out.println("Introduceti prima intrare booleana: ");
+
+        boolean primulBoolean = scanner.nextBoolean();
+
+        System.out.println("Introduceti a doua intrare booleana: ");
+
+        boolean alDoileaBoolean = scanner.nextBoolean();
+
+        boolean negarePrimulBoolean = !primulBoolean;
+        boolean negareAlDoileaBoolean = !alDoileaBoolean;
+
+        System.out.println("Negarea primului boolean: " + negarePrimulBoolean);
+        System.out.println("Negare al doilea boolean: " + negareAlDoileaBoolean);
+
+        scanner.close();
+    }
+}
