32d42277dd823c6301407d3077f4510dab252f48
Sebastian Mutulescu <sebastian.mutulescu9@gmail.com>
2024-10-28 17:09:34 +0200

    Tema JPA

--- ExercitiuChallengeCurs.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    Users.username,
-    AVG(LENGTH(Messages.content)) AS average_message_length
-FROM Users
-JOIN Messages ON Users.user_id = Messages.user_id
-GROUP BY Users.username
-HAVING AVG(LENGTH(Messages.content)) > 50;
---
+++ JPA/src/main/java/com/jpaHomework1/JPA/JpaApplication.java
@@ -0,0 +1,15 @@
+package com.jpaHomework1.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpaApplication.class, args);
+
+	}
+
+}
